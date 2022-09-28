529a42507dd8b379aa85808346124fb8686ed2c4
MYGUDEV <pguzmanit@gmail.com>
2022-09-27 21:23:04 -0300

    generate variable tempTherm without success

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ LOCATION = document.querySelector("button"),
 clima = wrapper.querySelector(".weather-part"),
 wIcon = clima.querySelector("img");
 
-// Variable que guradará la url con la key integrada
+// Variable que guradará la url con la key integrada.
 let api;
 
 // Función para luego de presionar enter en el input de CITY, procesa dicha CITY y hace el request a la API.
@@ -36,14 +36,14 @@ LOCATION.addEventListener("click", () =>{
 
 // Función request a la API por CITY, city como parámetro de la misma. Luego llama a la función fetchData.
 function solicitudApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=d8df573a71f861d27a7a93e6e190e8a0`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=659448f530b1011e83f181f7e97df12e`;
     fetchData();
 };
 
 // Función request a la API por coordenadas, variable position como parámetro de la misma. Position contiene las coordenadas obtenidas por IP del browser. Luego llama a la función fetchData.
 function validPosition(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=d8df573a71f861d27a7a93e6e190e8a0`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=659448f530b1011e83f181f7e97df12e`;
     fetchData();
 };
 
@@ -58,7 +58,7 @@ function fetchData(){
     infoTxt.innerText = "Extrayendo datos del clima de la región...";
     infoTxt.classList.add("pending");
     fetch(api).then(res => res.json()).then(result => weatherData(result)).catch(() =>{
-        infoTxt.innerText = "UPS! alguna falla sucedió";
+        infoTxt.innerText = "UPS! alguna falla surgió";
         infoTxt.classList.replace("Pendiente", "error");
     });
 };
@@ -67,6 +67,8 @@ function fetchData(){
 // Obtenemos información de API del clima y la almacenamos en las siguientes variables. La fución weatherData despliega en el DOM los datos obtenidos de la API api.openweathermap.org.
 
 // "const {..,..,..}=...;" esta alternativa de asignación crea variables con el mismo nombre de propiedad en el objeto que queremos extraer los valores correspondientes de cada propiedad, como resultado quedan disponibles las variables en forma individual con el mismo nombre de la propiedad que contenía el objeto de origen, así se evita repetir código al tipear la sintaxis "const" el numero de veces por cada valor de las propiedades a extraer en un mismo objeto.
+
+let tempTherm=0;
 function weatherData(data){
     if(data.cod == "404"){
         infoTxt.classList.replace("Pendiente", "error");
@@ -77,10 +79,10 @@ function weatherData(data){
         const city = data.name;
         const country = data.sys.country;
         const {description, id} = data.weather[0];
-        const {feels_like, humidity} = data.main;
-        const temperature = data.main;
+        const {temp, feels_like, humidity} = data.main;
+        tempTherm = temp;
 
-        clima.querySelector(".temp .numb").innerText = Math.floor(temperature);
+        clima.querySelector(".temp .numb").innerText = Math.floor(temp);
         clima.querySelector(".desc").innerText = description;//no supe como traducir la descripccion que manda la api de ingles
         clima.querySelector(".lugar span").innerText = `${city}, ${country}`;
         clima.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
@@ -89,18 +91,29 @@ function weatherData(data){
         infoTxt.innerText = "";
         CITY.value = "";
         wrapper.classList.add("active");
-        return temperature;
-    }
+        console.log(temp);
+        console.log(tempTherm);
+        return tempTherm;
+    };
+    console.log(temp);
+    console.log(tempTherm);
+    //return tempTherm;
+    
 };
+console.log(tempTherm);
+console.log(temp);
+// SE ME ESTÁ COMPLICANDO DE CÓMO LOGRAR DISPONIBILIDAD DEL DATO TEPERATURA DE LA CONSTANTE O LA VARIABLE temp y tempTherm RESPECTIVAMENTE
+
 
 // ---------------------------------------------------------
 
 // Ciclo para crear termómetro gráfico mediante css.
 // Convertimos a enteros los °C.
-let tempInt = clima.querySelector(".temp").innerText = Math.floor(temperature);//nos está fallando esta conversión de los °C, no reconoce la constante temperature.
+let tempInt = tempTherm;
+//nos está fallando esta conversión de los °C, no reconoce la constante temp.
 
 // generamos una variable para alternar incremento o decremento en el ciclo for.
-tempDownUp = (tempInt >= 0) ? (+1) : (-1) ;
+// tempDownUp = (tempInt >= 0) ? (+1) : (-1) ;
 
 // Función para adicionar elemento en el html.
 function addElemento(texto){
@@ -108,24 +121,23 @@ function addElemento(texto){
     var h1 = document.createElement("h1");
     h1.innerHTML = texto;
     capa.appendChild(h1);
-  }
-
+};
 
 // Iteración por cada °C para generar columna en colores mediante css.
-for (let temp = 0; temp <= tempInt; temp +tempDownUp) {
+for (let temp = 0; temp <= tempInt; temp = (tempInt >= 0) ? (+1) : (-1)) {
     if( temp <(-20)) {
-        addElemento('<div class="dangFreezz"></div>')
+        '<div class="dangFreezz"></div>'
     } else if ( temp >=(-20)&&temp<(-10)) {        
-        addElemento('<div class="freezz"></div>')
+        '<div class="freezz"></div>'
     } else if ( temp >=(-10)&&temp<(0)) {        
-        addElemento('<div class="cool"></div>')
+        '<div class="cool"></div>'
     } else if ( temp >=(0)&&temp<(15)) {        
-        addElemento('<div class="fresh"></div>')
+        '<div class="fresh"></div>'
     } else if ( temp >=(15)&&temp<(30)) {        
-        addElemento('<div class="templ"></div>')
+        '<div class="templ"></div>'
     } else if ( temp >=(30)&&temp<(45)) {        
-        addElemento('<div class="heat"></div>')
+        '<div class="heat"></div>'
     } else {        
-        addElemento('<div class="dangHeat"></div>')
+        '<div class="dangHeat"></div>'
     };
 } ;
